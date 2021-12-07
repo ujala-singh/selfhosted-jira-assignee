b285f1e6951347537fe9523d9e835ffd3d9ce9f8
ujala-singh <ujala.singh@razorpay.com>
2021-12-08 00:09:58 +0530

    Initial Commit

---
+++ action.js
@@ -0,0 +1,25 @@
+const Jira = require('./common/net/Jira')
+
+module.exports = class {
+  constructor ({ githubEvent, argv, config }) {
+    this.Jira = new Jira({
+      baseUrl: config.baseUrl,
+      token: config.token,
+      email: config.email,
+    })
+
+    this.config = config
+    this.argv = argv
+    this.githubEvent = githubEvent
+  }
+
+  async execute () {
+    const issueId = this.argv.issue || this.config.issue || null
+    const { assignee } = this.argv
+
+    console.log(`Adding assignee to ${issueId}: \n${assignee}`)
+    await this.Jira.addAssignee(issueId, { body: assignee })
+
+    return {}
+  }
+}
